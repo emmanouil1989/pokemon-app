f94db3d271a8f310ca1883d077dce302342a453f
emmanouil1989 <emmanouil.e25@gmail.com>
2022-09-20 13:02:03 +0100

    change styles of pokemon list page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,5 +2,9 @@ import React, { PropsWithChildren } from "react";
 
 type LayoutProps = PropsWithChildren<{}>;
 export default function Layout({ children }: LayoutProps) {
-  return <div>{children}</div>;
+  return (
+    <main className="h-screen w-screen flex flex-col justify-between items-center relative p-8 ">
+      {children}
+    </main>
+  );
 }
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -3,13 +3,19 @@ import { Pokemon } from "@prisma/client";
 import React from "react";
 import LoadingIndicator from "./LoadingIndicator";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function PokemonList() {
   const { data, isLoading, isSuccess } = trpc.useQuery(["search.pokemon"]);
+
+  const router = useRouter();
+  const onImageClick = (id: number) => {
+    router.push(`/pokemon/${id}`);
+  };
   return (
     <div
-      className={`flex flex-col justify-center "h-full" 
-       w-full  items-center p-8`}
+      className={`flex flex-col justify-center h-full 
+       w-full  items-center p-8 relative `}
     >
       {isLoading || !isSuccess ? (
         <LoadingIndicator />
@@ -17,8 +23,8 @@ export default function PokemonList() {
         <>
           <h1 className="text-3xl font-bold pb-8">Pokemon list</h1>
 
-          <ul className="grid xl:grid-cols-[150px_150px_150px_150px] lg:grid-cols-[150px_150px_150px] md:grid-cols-[150px_150px] grid-cols-[200px]  xl:auto-rows-[200px] lg:auto-rows-[200px]  md:auto-rows-[200px] auto-rows-[300px]  gap-8  h-full">
-            {data.pokemons.map(({ name, spriteUrl }: Pokemon) => (
+          <ul className="grid xl:grid-cols-[150px_150px_150px_150px] lg:grid-cols-[150px_150px_150px] md:grid-cols-[150px_150px] grid-cols-[200px]  xl:auto-rows-[200px] lg:auto-rows-[200px]  md:auto-rows-[200px] auto-rows-[300px]  gap-8  h-full list-inside overflow-auto p-8">
+            {data.pokemons.map(({ name, spriteUrl }: Pokemon, index) => (
               <li key={name} className="h-full w-full  flex justify-center">
                 <div className=" h-full w-full relative shadow-md shadow-cyan-100 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 aspect-w-1 aspect-h-1 overflow-hidden rounded-lg  xl:aspect-w-7 xl:aspect-h-8">
                   <button className="relative h-full w-full">
@@ -27,7 +33,7 @@ export default function PokemonList() {
                       src={spriteUrl}
                       alt={name}
                       layout="fill"
-                      onClick={() => console.log("clicked")}
+                      onClick={() => onImageClick(index)}
                     />
                   </button>
                 </div>
